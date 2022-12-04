ea47707aa29713d9a2c4d135c23273e88ed21d68
李应 <2439175930@qq.com>
2022-12-04 15:40:42 +0800

    init: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,12 @@ import React, { ReactElement } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { Button } from 'react-vant'
+import { useRoutes } from 'react-router'
+import RouterConfig from 'src/lib/routers'
 
 function App(): ReactElement {
-  const APP_VERSION = 'v1.0.0'
-  console.log('123')
-  return (
-    <div className='App'>
-      <div className='demo-button'>
-        <Button type='primary'>Primary</Button>
-        <Button type='info'>Info</Button>
-        <Button type='default'>Default</Button>
-        <Button type='warning'>Warning</Button>
-        <Button type='danger'>Dangeer</Button>
-        <input type='text' />
-      </div>
-      <header className='App-header'>
-        <img alt='logo' className='App-logo' src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  const element = useRoutes(RouterConfig)
+  return <div className='App'>{element}</div>
 }
 
 export default App
---
+++ src/lib/routers.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from 'pages/home/index'
+
+const RouterConfig = [
+  {
+    path: '/home',
+    element: <Home />
+  }
+]
+
+export default RouterConfig
