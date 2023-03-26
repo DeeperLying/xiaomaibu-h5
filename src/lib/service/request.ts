153a20576c2f8736e75ad2fbc5f1bc8361d0f5fa
李应 <2439175930@qq.com>
2023-03-26 11:53:41 +0800

    fix: base source

--- src/lib/service/request.ts
+++ src/lib/service/request.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Lee
  * @Date: 2022-12-04 19:02:04
- * @LastEditTime: 2023-03-11 12:58:16
+ * @LastEditTime: 2023-03-26 11:51:38
  * @LastEditors: Lee
  */
 import Request from './conf'
@@ -24,8 +24,8 @@ interface YWZRequestConfig<T, R> extends RequestConfig<Response<R>> {
 }
 
 const request = new Request({
-  //baseURL: 'http://blog.xiaomaibu.pro/api/',
-  baseURL: 'http://127.0.0.1:8443/api/',
+  baseURL: 'http://blog.xiaomaibu.pro/api/',
+  //baseURL: 'http://127.0.0.1:8443/api/',
   timeout: 1000 * 60 * 5,
   headers: {
     Authentication: Cookies.get('Token'),
