8b36828a2be425e865d31973637de2687c5cb501
李应 <2439175930@qq.com>
2023-04-01 18:00:34 +0800

    fix: no style

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,14 @@
 /*
  * @Author: Lee
  * @Date: 2023-03-25 23:37:44
- * @LastEditTime: 2023-04-01 17:31:29
+ * @LastEditTime: 2023-04-01 17:59:27
  * @LastEditors: Lee
  */
 
 import React, { memo } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Tabbar } from 'react-vant/lib/tabbar'
+import { Tabbar } from 'react-vant/'
 import { HomeO, Search } from '@react-vant/icons'
 
 const Menu = () => {
