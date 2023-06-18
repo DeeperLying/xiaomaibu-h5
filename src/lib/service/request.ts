ffcd8ea85101c72b4629489bb27c69058911034d
李应 <2439175930@qq.com>
2023-06-18 15:20:16 +0800

    feat: update base

--- src/lib/service/request.ts
+++ src/lib/service/request.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Lee
  * @Date: 2022-12-04 19:02:04
- * @LastEditTime: 2023-06-18 14:47:05
+ * @LastEditTime: 2023-06-18 15:19:57
  * @LastEditors: Lee
  */
 import Request from './conf'
@@ -24,7 +24,7 @@ interface YWZRequestConfig<T, R> extends RequestConfig<Response<R>> {
 }
 
 const request = new Request({
-  baseURL: 'http://blog.xiaomaibu.pro/api/',
+  baseURL: 'http://shop.xiaomaibu.pro/api/',
   //baseURL: 'http://127.0.0.1:8443/api/',
   timeout: 1000 * 60 * 5,
   headers: {
